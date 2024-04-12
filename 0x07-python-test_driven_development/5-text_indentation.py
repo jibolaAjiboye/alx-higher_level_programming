ef967fee4950fa4c328d280a03a9b177da11e52a
Ifeoluwa2001 <ajiboyeifeoluwa100@gmail.com>
2024-04-12 05:04:56 -0700

    =-=-=

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""Defining a function text_indentation"""
+
+"""
+The module is text_indentation
+prints a text with 2 new lines after . ? and :
+"""
 
 
 def text_indentation(text):
-    """Prints Text with 2 lines after ['.', '?', and ':'.]
-
-    Args:
-        text (string): Printed Text.
-    Raises:
-        TypeError: Text not string.
     """
-    if not isinstance(text, str):
+    prints a text
+    args:
+        text(string)
+    no space at the beginning or end of each printed line
+    """
+
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
 
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+    print(f"{text}", end="")
